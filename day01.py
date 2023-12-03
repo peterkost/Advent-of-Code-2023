f9b9589f4bc231e6ac55ebaef4f15c4562a07306
peterkost <46546031+peterkost@users.noreply.github.com>
2023-12-03 13:06:32 -0500

    Soved Day 1 part 2

--- day01.py
+++ day01.py
@@ -1,3 +1,5 @@
+PART1 = False
+
 def getLines() -> list[str]:
     input = open("day01-input.txt", "r")
     return input.readlines()
@@ -15,10 +17,29 @@ def getValueOf(line: str) -> int:
 def getDigit(line: str, first: bool) -> str:
     rnge = range(len(line)) if first else range(len(line) - 1, -1, -1)
     for i in rnge:
-        if line[i].isnumeric():
-            return line[i]
+        cur = digit(line, i)
+        if cur[0]:
+            return cur[1]
     raise Exception(f"Couldn't find digit {'first' if first else 'last'} digit in {line}")
 
+# returns [isDigit, digitAsNumeralString]
+def digit(line: str, i) -> tuple[bool, str]:
+    if PART1 or line[i].isnumeric():
+        return (line[i].isnumeric(), line[i])
+    else:
+        return isWordADigit(line, i) 
+
+# PART 2
+FIRST_LETTERS = set(["o", "t", "f", "s", "e", "n"])
+POSSIBLE_NUMS = {"o":["one"], "t":["two", "three"], "f":["four", "five"], "s":["six", "seven"], "e":["eight"], "n":["nine"]}
+WORD_TO_NUM = {"one": "1", "two": "2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+def isWordADigit(line: str, i) -> tuple[bool, str]:
+    if line[i] in FIRST_LETTERS:
+        for number in POSSIBLE_NUMS[line[i]]:
+            end = i + len(number)
+            if i + len(number) <= len(line) and line[i:end] == number:
+                return (True, WORD_TO_NUM[number])
+    return (False, "")
 
 lines = getLines()
 sum = sumCalibrationValues(lines)
