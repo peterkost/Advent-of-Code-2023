6450916794906e4c96f8c91ec118abbcc395b8af
peterkost <46546031+peterkost@users.noreply.github.com>
2023-12-05 20:58:43 -0500

    Solved day 4 part 2

--- day04/part1.py
+++ day04/part1.py
@@ -1,28 +1,34 @@
 import re
+from typing import List, Set, Tuple
+from collections import defaultdict
 from utils.fileHelper import getLinesFor
 
+
+def getWinningAndMyNumsFor(game: str) -> Tuple[Set[str], List[str]]:
+    nums = re.sub(r'.*: ', '', game.strip())
+    winningStr, myStr = nums.split(' | ')
+    return (set(winningStr.split()), myStr.split())
+
+def getNumWinnersIn(game: str) -> int:
+    wins = 0
+    winningNums, myNums = getWinningAndMyNumsFor(game)
+    for num in myNums:
+        if num in winningNums:
+            wins += 1
+    return wins
 
 def getAndPrintAnswer():
     lines = getLinesFor(day="04", sample=False)
 
-    sum = 0
+    instancesOf = defaultdict(int)
+    gameNum = 1
     for game in lines:
-        nums = re.sub(r'.*: ', '', game.strip())
-        winningStr, myStr = nums.split(' | ')
-        winningNums, myNums = winningStr.split(), myStr.split()
-        
-        gameTotal = 0
-        for num in myNums:
-            if num in winningNums:
-                if not gameTotal:
-                    gameTotal = 1
-                else:
-                    gameTotal *= 2
-        sum += gameTotal
-
-    print(sum)
-
+        instancesOf[gameNum] += 1
+        wins = getNumWinnersIn(game)
+        for i in range(gameNum + 1, gameNum + wins + 1):
+            instancesOf[i] += instancesOf[gameNum]
+        gameNum += 1
+    print(sum(instancesOf.values()))
 
 if __name__ == "__main__":
    getAndPrintAnswer()
-
---
+++ day04/part2.py
@@ -0,0 +1,27 @@
+import re
+from utils.fileHelper import getLinesFor
+
+
+def getAndPrintAnswer():
+    lines = getLinesFor(day="04", sample=False)
+
+    sum = 0
+    for game in lines:
+        nums = re.sub(r'.*: ', '', game.strip())
+        winningStr, myStr = nums.split(' | ')
+        winningNums, myNums = winningStr.split(), myStr.split()
+        
+        gameTotal = 0
+        for num in myNums:
+            if num in winningNums:
+                if not gameTotal:
+                    gameTotal = 1
+                else:
+                    gameTotal *= 2
+        sum += gameTotal
+
+    print(sum)
+
+
+if __name__ == "__main__":
+   getAndPrintAnswer()
