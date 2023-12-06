59f5733ae4679ad749a4ac1ceb07e9ca3bc99135
peterkost <46546031+peterkost@users.noreply.github.com>
2023-12-06 09:54:38 -0500

    Solved day 5 part 1

---
+++ day05/part1.py
@@ -0,0 +1,67 @@
+from typing import List, Tuple
+from utils.fileHelper import getLinesFor
+
+def seedToLocation(seed: int, chain: List[List[Tuple[int, int, int]]]) -> int:
+    cur = seed
+    for map in chain:
+        for interval in map:
+            start, end, change = interval
+            if start <= cur <= end:
+                cur += change
+                break
+    return cur
+
+def getSeeds(line: str) -> List[int]:
+    strSeeds = line.replace("seeds: ", "").split()
+    return [int(seed) for seed in strSeeds]
+
+def getChainOfMaps(lines: List[str]) ->List[List[Tuple[int, int, int]]]:
+    res = []
+    processedInput = getInputMapsAsList(lines)
+
+    for map in processedInput:
+        formatedMap = []
+        for e in map:
+            asRange = convertInputToRange(e)
+            formatedMap.append(asRange)
+        res.append(formatedMap)
+
+    return res
+
+def getInputMapsAsList(lines: List[str]) -> List[List[int]]:
+    res = []
+    i = 0
+    while i < len(lines):
+        curMap = []
+
+        while i < len(lines) and lines[i]:
+            if lines[i][0].isnumeric():
+                lineAsInt = [int(strNum) for strNum in lines[i].split()]
+                curMap.append(lineAsInt)
+            i += 1
+        i += 1
+        res.append(curMap)
+    return res
+
+def convertInputToRange(input: List[int]) -> Tuple[int, int, int]:
+    destStart, sourceStart, rangeLen = input
+    sourceEnd = sourceStart + rangeLen  - 1
+    change =  destStart - sourceStart
+
+    return (sourceStart, sourceEnd, change)
+
+
+def getAndPrintAnswer():
+    lines = getLinesFor(day="05", sample=False)
+
+    seeds = getSeeds(lines[0])
+    chainOfMaps = getChainOfMaps(lines[2:])
+    locations = []
+
+    for seed in seeds:
+        locations.append(seedToLocation(seed, chainOfMaps))
+
+    print(min(locations))
+
+if __name__ == "__main__":
+   getAndPrintAnswer()
--- utils/fileHelper.py
+++ utils/fileHelper.py
@@ -1,6 +1,6 @@
 def getLinesFor(day: str, sample: bool = False) -> list[str]:
     input = open(f"inputs/day{day}{'-sample' if sample else ''}.txt", "r")
-    return input.readlines()
+    return [line.strip() for line in input.readlines()]
 
 def writeListToFile(input: list, name: str):
     file = open(f"tmp/{name}.txt", "w")
