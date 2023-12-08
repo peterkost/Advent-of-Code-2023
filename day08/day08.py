16293714bbf610a89dbc845023e691742128485e
peterkost <46546031+peterkost@users.noreply.github.com>
2023-12-08 16:24:57 -0500

    Solved day 8 part 2

--- day08/day08.py
+++ day08/day08.py
@@ -2,38 +2,46 @@ from typing import List
 from utils.fileHelper import getLinesFor
 from day08.Node import Node
 import re
+from math import lcm
 
-def buildNodesFrom(lines: List[str]) -> Node:
+PART = 2
+
+def buildNodesFrom(lines: List[str]) -> List[Node]:
     nodeMap = {}
     for line in lines:
         name = line.split()[0]
         nodeMap[name] = Node(name)
 
-    r = re.compile("[A-Z]{3}")
+    r = re.compile("[A-Z0-9]{3}")
+    starts = []
     for line in lines:
         nodeName, left, right = r.findall(line)
         node = nodeMap[nodeName]
         node.left, node.right = nodeMap[left], nodeMap[right]
-    return nodeMap["AAA"]
+        if (PART == 1 and nodeName == "AAA") or (PART == 2 and nodeName[-1] == "A"):
+            starts.append(node)
+    return starts
 
 def countSteps(start: Node, moves):
     steps, cur, i = 0, start, 0
     n = len(moves)
-    while cur.name != "ZZZ":
+    while not isEnd(cur):
         move = moves[i % n]
         cur = cur.left if move == "L" else cur.right
         i += 1
         steps += 1
-
-
-
     return steps
+
+def isEnd(node: Node) -> bool:
+    return node.name == "ZZZ" if PART == 1 else node.name[-1] == "Z"
 
 def solve():
     lines = getLinesFor(day="08")
-    start = buildNodesFrom(lines[2:])
-    steps = countSteps(start, lines[0])
-    print(steps)
+    starts = buildNodesFrom(lines[2:])
+    steps = []
+    for start in starts:
+        steps.append(countSteps(start, lines[0]))
+    print(lcm(*steps))
 
 
 solve()
